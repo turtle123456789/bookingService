f33eb1d1614c8c5992dd70a73909d8164d30e43e
Pham Duc Quy <turtlebebautiful96@gmail.com>
2025-06-04 06:13:09 +0700

    a

--- be/src/models/booking.model.js
+++ be/src/models/booking.model.js
@@ -17,7 +17,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: 'Users',
+        model: 'users',
         key: 'id',
       }
     },
